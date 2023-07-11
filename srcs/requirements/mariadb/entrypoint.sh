7ec1041f44a04f3e7666a6e9a5182e67a76b4ed5
passionroro <rohoarau@student.42lausanne.ch>
2023-07-11 14:31:42 +0200

    nginx and wordpress look right : need to fix mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 
-eval "$(cat /usr/local/bin/db_create.sql)" | mariadb
+mysql_install_db
 
-/usr/bin/mysql_safe
+/etc/init.d/mysql start
 
+# Notes: secure_installation may require root password not encrypted
+# mariadb container doesn't have env variables 
+
+if [ -d "/var/lib/mysql/$MARIADB_DB" ]; then
+	echo "Database "${MARIADB_DB}" already exists."
+else
+	/usr/bin/mysql_secure_installation << EOF
+
+Y
+$MARIADB_ROOT_PWD
+$MARIADB_ROOT_PWD
+Y
+n
+Y
+Y
+EOF
+	
+	mysql -uroot -p$MARIADB_ROOT_PWD $MARIADB_DB < /usr/local/bin/db_create.sql
+fi
+
+/etc/init.d/mysql stop
+
+#You can start the MariaDB daemon with:
+#cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'  
+
+mysqld
